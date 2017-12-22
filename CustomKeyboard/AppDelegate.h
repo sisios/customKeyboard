64831b483599aa04ad0993d4fc675852312252ec
weisy@dehuinet.com <weisy@dehuinet.com>
2017-12-22 10:42:10 +0800

    add appdelegate.h

---
+++ CustomKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testtest
+//
+//  Created by 未思语 on 22/12/2017.
+//  Copyright © 2017 wsy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
