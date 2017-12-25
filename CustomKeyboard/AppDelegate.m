aba5584fedcb01b4692e90127f99ede9f7f8390f
weisy@dehuinet.com <weisy@dehuinet.com>
2017-12-25 09:11:08 +0800

    test

--- CustomKeyboard/AppDelegate.m
+++ CustomKeyboard/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,8 +19,8 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
-//    ViewController *vc = [[ViewController alloc]init];
-//    UINavigationController *navc = [[UINavigationController alloc]initWithRootViewController:vc];
+    ViewController *vc = [[ViewController alloc]init];
+    UINavigationController *navc = [[UINavigationController alloc]initWithRootViewController:vc];
 //    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
 //    self.window.rootViewController = navc;
 //    [self.window makeKeyAndVisible];
